8b3119097945073492610e08a61eb22ea71714e6
Bloodpressure2286 <139692696+Bloodpressure2286@users.noreply.github.com>
2025-07-23 09:37:59 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,7 @@
+-- DARKSTRIKE HUB (v0.1)
+-- Пока тут заглушка. Основной код будет добавлен позже.
+game.StarterGui:SetCore("SendNotification", {
+    Title = "DARKSTRIKE";
+    Text = "Чит загружен!";
+    Duration = 5;
+})
